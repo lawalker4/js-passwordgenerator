7e9d248bdd13ad33d0a61915f0617353c09e2227
LA WALKER <lauraashleywalker4@gmail.com>
2022-03-17 22:00:42 -0400

    update git

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,24 +1,26 @@
-var choiceArr = []; 
+var choiceArr = [];
 var characterLength = 8;
 
-var specialCharArr= ['!', '@', '#', '$','%',"^",'&','*','~'];
-var lowerCaseArr= ['a','s','d','f','g','h','j','k','l'];
-var upperCaseArr= ['Z','X','C','V','B','N','N','M','L']
-var numberArr=['1','2','3','4','5','6','7','8','9',"0"]
+var specialCharArr = ['!', '@', '#', '$', '%', "^", '&', '*', '~'];
+var lowerCaseArr = ['a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l'];
+var upperCaseArr = ['Z', 'X', 'C', 'V', 'B', 'N', 'N', 'M', 'L']
+var numberArr = ['1', '2', '3', '4', '5', '6', '7', '8', '9', "0"]
 
 // Assignment code here
-
-
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+    //need to call from prompts to generate password
 
-  passwordText.value = password;
+    if (correctPrompts) {
+
+    var password = generatePassword();
+    var passwordText = document.querySelector("#password");
+
+    passwordText.value = password;
 
 }
 
@@ -29,29 +31,34 @@ generateBtn.addEventListener("click", writePassword);
 function generatePassword() {
     //Generate password based on prompts
 
+    var password = "";
+    for (var i =0; i < characterLength; i++) {
+        var randomLetter
+    }
+
 }
 
-function getPrompts(){
-    characterLength = parseInt(prompt("Home many characters do you wnt your password to be? (8-128 characters")); 
+function getPrompts() {
+    characterLength = parseInt(prompt("Home many characters do you wnt your password to be? (8-128 characters"));
 
-    if(characterLength < 8 || characterLength > 128) {
-        alert ("Character length must be a number 8-128. Try Again!");
+    if (characterLength < 8 || characterLength > 128) {
+        alert("Character length must be a number 8-128. Try Again!");
         return false;
     }
 
     if (confirm("Would you like special characters in your new password?")) {
-        choiceArr = choiceArr.concat (specialCharArr);
+        choiceArr = choiceArr.concat(specialCharArr);
     }
 
     if (confirm("Would you like lower case letters in your new password?")) {
-        choiceArr = choiceArr.concat (lowerCaseArr);
+        choiceArr = choiceArr.concat(lowerCaseArr);
     }
 
     if (confirm("Would you like upper case letters in your new password?")) {
-        choiceArr = choiceArr.concat (upperCaseArr);
+        choiceArr = choiceArr.concat(upperCaseArr);
     }
     if (confirm("Would you like numbers in your new password?")) {
-        choiceArr = choiceArr.concat (numberArr);
+        choiceArr = choiceArr.concat(numberArr);
 
     }
     return true;
